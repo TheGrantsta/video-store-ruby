aa868e7f6b0bf893cf9f1e3cf81c7e0adf4acc1f
Grant McKenna <grant.mckenna@madetech.com>
2020-10-15 11:26:12 +0100

    Amend movie_spec to include new releases and regular movie expectations

--- movie_spec.rb
+++ movie_spec.rb
@@ -1,7 +1,18 @@
 require_relative "movie"
 
 RSpec.describe Movie do
+  it "should set up a new release movie object with the appropriate values" do
+    movie = Movie.new "New release movie title", "NewRelease"
 
+    expect(movie.title).to eq "New release movie title"
+    expect(movie.type).to eq "NewRelease"
+  end
+  it "should set up a regular movie object with the appropriate values" do
+    movie = Movie.new "Regular movie title", "Regular"
+
+    expect(movie.title).to eq "Regular movie title"
+    expect(movie.type).to eq "Regular"
+  end
   it "should set up a children's movie object with the appropriate values" do
     movie = Movie.new "Children's movie title", "Child"
 
