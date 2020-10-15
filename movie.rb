0a2e97b14057923c50b2b2d6476801a40778912c
Grant McKenna <grant.mckenna@madetech.com>
2020-10-15 11:33:43 +0100

    Amend movie to ensure the type is in the supported list

--- movie.rb
+++ movie.rb
@@ -1,8 +1,15 @@
 class Movie
   attr_reader :title, :type
-  
+
+  TYPES = ["NewRelease", "Regular", "Child"]
+
   def initialize title, type
     @title = title
-    @type = type
+
+    if TYPES.include? type
+      @type = type
+    else
+      @type = "UNKNOWN"
+    end
   end
 end
--- movie_spec.rb
+++ movie_spec.rb
@@ -1,6 +1,26 @@
 require_relative "movie"
 
 RSpec.describe Movie do
+  it "should check that the type is supported - Child" do
+    movie = Movie.new "Children's movie title", "Child"
+
+    expect(movie.type).to eq "Child"
+  end
+  it "should check that the type is supported - NewRelease" do
+    movie = Movie.new "New release movie title", "NewRelease"
+
+    expect(movie.type).to eq "NewRelease"
+  end
+  it "should check that the type is supported - Regular" do
+    movie = Movie.new "Regular movie title", "Regular"
+
+    expect(movie.type).to eq "Regular"
+  end
+  it "should check that the type is not supported - Children" do
+    movie = Movie.new "Children's movie title", "Children"
+
+    expect(movie.type).to eq "UNKNOWN"
+  end
   it "should set up a new release movie object with the appropriate values" do
     movie = Movie.new "New release movie title", "NewRelease"
 
