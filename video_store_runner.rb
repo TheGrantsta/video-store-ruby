4e9c7ed11f0e0606599bdc1431e7ef25084b0e3a
Grant McKenna <grant.mckenna@madetech.com>
2020-10-15 17:13:04 +0100

    Amend video runner to include movie of each type

--- video_store_runner.rb
+++ video_store_runner.rb
@@ -4,11 +4,13 @@ require_relative "rental"
 m1 = Movie.new "Harry Potter and the Prisoner of Azkaban", "Child"
 m2 = Movie.new "Harry Potter and the Goblet of Fire", "Child"
 m3 = Movie.new "James Bond - Quantum of Solace", "Regular"
-@rentals = []
+m4 = Movie.new "Trent", "NewRelease"
 
+@rentals = []
 @rentals.push Rental.new m1, 4
 @rentals.push Rental.new m2, 2
 @rentals.push Rental.new m3, 1
+@rentals.push Rental.new m4, 3
 
 statement = Statement.new "Bob The Builder", @rentals
 
