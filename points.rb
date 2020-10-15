4eada373e624e65d5a59a3741e67634b209352a4
Grant McKenna <grant.mckenna@madetech.com>
2020-10-15 12:36:29 +0100

    Amend project to include class for calculating points for new release movies

---
+++ points.rb
@@ -0,0 +1,9 @@
+require_relative "movie"
+
+class Points
+  def self.calculate movie, days
+    @points = 1
+
+    @points
+  end
+end
---
+++ points_spec.rb
@@ -0,0 +1,18 @@
+require_relative "points"
+
+RSpec.describe Points do
+  describe "new release movies are only ones with special logic" do
+    before (:all) do
+      @new_release_movie = Movie.new "New release movie title", "NewRelease"
+    end
+  it "should return one point for one day rental" do
+    days = 1
+    points = Points.calculate @new_release_movie, days
+
+    expect(points).to eq 1
+  end
+  it "should return two points per day after first day of rental" do
+
+  end
+  end
+end
