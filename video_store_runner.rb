41445499f46eaa44bc45ca04edb290cc63285400
Grant McKenna <grant.mckenna@madetech.com>
2020-10-15 14:18:50 +0100

    Amend project to include video store runner

---
+++ video_store_runner.rb
@@ -0,0 +1,10 @@
+require_relative "statement"
+
+statement = Statement.new "Bob The Builder"
+
+puts "*******************************\n"
+puts "#{statement}"
+
+puts
+puts "*******************************\n"
+puts
