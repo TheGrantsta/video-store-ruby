1a3639e423b647976a30f8603056ac1bb52eedcf
Grant McKenna <grant.mckenna@madetech.com>
2020-10-15 12:08:30 +0100

    Refactor: replace private methods with one private method

--- cost.rb
+++ cost.rb
@@ -2,28 +2,20 @@ require_relative "movie"
 
 class Cost
   @cost = 0;
+
   def self.calculate movie, days
     if movie.type == "NewRelease"
-      calculate_new_release_rental days
+      @cost = days * 3
     elsif movie.type == "Regular"
-      calculate_regular_rental days
+      calculate_cost_with_discount 2, days, 2
     else
-      calculate_child_movie_rental days
+      calculate_cost_with_discount 1.5, days, 3
     end
 
     @cost
   end
 
   private
-  def self.calculate_new_release_rental days
-      @cost = days * 3
-  end
-  def self.calculate_regular_rental days
-    calculate_cost_with_discount 2, days, 2
-  end
-  def self.calculate_child_movie_rental days
-    calculate_cost_with_discount 1.5, days, 3
-  end
   def self.calculate_cost_with_discount initialCost, days, discountDays
     @cost = initialCost
 
