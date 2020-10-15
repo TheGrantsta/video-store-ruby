8b8f82986074b214c540658b724d28e2308ad7c8
Grant McKenna <grant.mckenna@madetech.com>
2020-10-15 12:43:15 +0100

    Amend points_spec to tst for regular and children movies

--- points_spec.rb
+++ points_spec.rb
@@ -18,4 +18,38 @@ RSpec.describe Points do
       expect(points).to eq 9
     end
   end
+  describe "regular movies accur one point" do
+    before (:all) do
+      @movie = Movie.new "New release movie title", "Regular"
+    end
+    it "should return one point" do
+      days = 1
+      points = Points.calculate @movie, days
+
+      expect(points).to eq 1
+    end
+    it "should return one point - irrespective of the length of rental" do
+      days = 5000
+      points = Points.calculate @movie, days
+
+      expect(points).to eq 1
+    end
+  end
+  describe "children movies accur one point" do
+    before (:all) do
+      @movie = Movie.new "New release movie title", "Child"
+    end
+    it "should return one point" do
+      days = 1
+      points = Points.calculate @movie, days
+
+      expect(points).to eq 1
+    end
+    it "should return one point - irrespective of the length of rental" do
+      days = 5000
+      points = Points.calculate @movie, days
+
+      expect(points).to eq 1
+    end
+  end
 end
